b66cd1ef38baa90e9c5859e79dbcfa514a6ea194
XresuriX <92220727+XresuriX@users.noreply.github.com>
2023-03-05 07:44:14 -0800

    reworking everything

--- GrandMarket/Stalls/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from .models import Stall
-
-
-class StallFilter(django_filters.FilterSet):
-
-    class Meta:
-        model = Stall
-        fields = ['category', 'primary_delivery_location']
---
+++ src/apps/partner/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from oscar.apps.partner.abstract_models import AbstractPartner
+
+class Partner(AbstractPartner):
+    primary_delivery_location = models.CharField(max_length=150, blank=False, null=False, default='kingston')
+    secondary_delivery_location = models.CharField(max_length=150, blank=True, null=True)
+    contact_number = models.CharField(max_length=20, null=True, blank=True)
+
+
+from oscar.apps.partner.models import *  # noqa isort:skip
