859550390dac049089995ffd782f69b175ef5cdf
XresuriX <92220727+XresuriX@users.noreply.github.com>
2023-03-06 08:41:56 -0800

    committing brfore partner migration

--- src/apps/partner/models.py
+++ src/apps/partner/models.py
@@ -1,10 +1,51 @@
 from django.db import models
 from oscar.apps.partner.abstract_models import AbstractPartner
+from oscar.core.utils import slugify
+from django.utils.translation import gettext as _
+
+
+
+
+class PartnerGroup(models.Model):
+    name = models.CharField(_('Name'), max_length=100, unique=True)
+    slug = models.SlugField(_('Slug'), max_length=100, unique=True)
+
+    class Meta:
+        app_label = 'partners'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
 
 class Partner(AbstractPartner):
     primary_delivery_location = models.CharField(max_length=150, blank=False, null=False, default='kingston')
     secondary_delivery_location = models.CharField(max_length=150, blank=True, null=True)
     contact_number = models.CharField(max_length=20, null=True, blank=True)
+    image = models.ImageField(
+        _("Image"),
+        upload_to="images/partner-images",
+        blank=True, null=True)
+    group = models.ForeignKey(
+        'PartnerGroup',
+        models.PROTECT,
+        related_name='partners',
+        verbose_name=_("Group"),
+        null=True,
+        blank=True
+    )
+
+    is_pickup_store = models.BooleanField(_("Is pickup store"), default=True)
+    is_active = models.BooleanField(_("Is active"), default=True)
+
+
+
+
 
 
 
